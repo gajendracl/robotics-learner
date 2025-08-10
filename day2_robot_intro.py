1be98adc534fcd84c91dba2fc76d0398c8e4e03c
gajendracl <cloud.gajendra@gmail.com>
2025-08-10 12:27:32 +0530

    Day 2 - Interactive Robot Intro program

---
+++ day2_robot_intro.py
@@ -0,0 +1,4 @@
+print("🤖 Hello, I am RoboBuddy, your training assistant.")
+user_name = input("What's your name engineer?")
+mission_count = int(input("How many missions have you completed? "))
+print(f"Nice to meet you, {user_name}! You've completed {mission_count} missions. Let's get started!")
